8294e4e1a7bccd66c3d94548a02fea11828449cc
Justin Lee <2025jlee@tjhsst.edu>
2023-12-21 22:39:10 -0500

    basic website

---
+++ website/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*.html", "./templates/**/*.html", "./static/*.css", "./static/**/*.css"],
+  theme: {
+    extend: [],
+  },
+  plugins: [],
+}
+
