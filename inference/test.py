a34156d07548b0885f4882dddbccffd566e58bf0
Alan Zhu <2025azhu@tjhsst.edu>
2024-01-01 16:37:20 -0500

    make arm code work

--- inference/test.py
+++ inference/test.py
@@ -5,7 +5,31 @@ arm = xarm.Controller("USB")
 
 print(arm.getBatteryVoltage())
 
-arm.setPosition(0, 20, wait=False)
+# 1: claw pincers
+# 2: claw rotate
+# 3: arm top
+# 4: arm middle
+# 5: arm bottom
+# 6: base
+
+LIMITS = {
+    1: (1150, 2300, 1500),  # close
+    2: (400, 2600, 1500),   # counterclockwise
+    3: (400, 1900, 400),    # down
+    4: (400, 2600, 1500),   # down
+    5: (1200, 2600, 2300),  # up
+    6: (400, 2600, 1900),   # counterclockwise
+}
+
+
+input("Moving servos to default positions. Press enter to continue...")
+
+for servo, limits in LIMITS.items():
+    upper, lower, default = limits
+    arm.setPosition(servo, default)
+
 
 while True:
-    time.sleep(0.1)
+    s = input("Enter servo number: ")
+    arm.setPosition(int(s), int(input("Enter position: ")))
+    print()
