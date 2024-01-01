f8437ca7be913c333fdccae5a216e016cc92f374
Alan Zhu <2025azhu@tjhsst.edu>
2024-01-01 17:22:33 -0500

    small adjustment

--- inference/test.py
+++ inference/test.py
@@ -14,7 +14,7 @@ print(arm.getBatteryVoltage())
 
 LIMITS = {
     1: (1150, 2300, 1500),  # close
-    2: (400, 2600, 1500),   # counterclockwise
+    2: (400, 2600, 520),   # counterclockwise
     3: (400, 1900, 400),    # down
     4: (400, 2600, 1500),   # down
     5: (1200, 2600, 2300),  # up
@@ -26,7 +26,7 @@ input("Moving servos to default positions. Press enter to continue...")
 
 for servo, limits in LIMITS.items():
     upper, lower, default = limits
-    arm.setPosition(servo, default)
+    arm.setPosition(servo, default, wait=False)
 
 
 while True:
