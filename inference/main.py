5fdb08144a2a7e07ab04b405830282ecb16acb78
Justin Lee <2025jlee@tjhsst.edu>
2024-01-07 21:05:48 -0500

    working code

--- inference/main.py
+++ inference/main.py
@@ -17,7 +17,7 @@ from sklearn.linear_model import LinearRegression
 # 6: base
 
 LIMITS = {
-    1: ((1150, 90), (2300, 180), 1500),  # close
+    1: ((1150, 90), (2300, 180), 1150),  # close
     2: ((400, -180), (2600, 200), 1500),  # counterclockwise
     3: ((410, 10), (1900, 140), 410),  # down
     4: ((400, -100), (2600, 100), 1500),  # down
@@ -32,24 +32,25 @@ POINTS = {
     1: ( (1150, 90),   (2300, 180),              ),
     2: ( (400, -180),  (2600, 200),              ),
     3: ( (1275, 90),   (750, 45),   (1750, 135), ),
-    4: ( (1500, 0),    (1975, 45),  (2425, 90),  ),
+    4: ( (1500, 0),    (1975, 45),  (2475, 90),  ),
     5: ( (2325, 0),    (1850, 45),  (1325, 90),  ),
-    6: ( (1900, 0),    (875, 45),                ),
+    6: ( (1900, 0),    (875, 90),                ),
 }
 # fmt: on
 
 
 H = 14.5  # cm
 L1 = 10.5
+L_3_1 = 1.7
 L2 = 9
 L3 = 17
 
 # width, height of Area that camera sees
 # As well as the location of the arm origin relative to the bottom left of the camera vision
-AREA_W = 53
-AREA_H = 45.7
-ARM_X = 0
-ARM_Y = 18.5
+AREA_W = 52
+AREA_H = 41
+ARM_X = -3
+ARM_Y = 41 - 31
 
 M3_BOUNDS = (0, 3 / 4 * math.pi)
 M4_BOUNDS = (0, 7 / 12 * math.pi)
@@ -60,9 +61,9 @@ BOUNDS = [M3_BOUNDS, M4_BOUNDS, M5_BOUNDS, M6_BOUNDS]
 CURRENT_POSITIONS = {i: None for i in range(1, 7)}
 
 # fmt: off
-EQ_X = lambda m3, m4, m5, m6: math.cos(m6) * (math.sin(m5) * L1 + math.sin(m4 + m5) * L2 + math.sin(m3 + m4 + m5) * L3)
-EQ_Y = lambda m3, m4, m5, m6: math.sin(m6) * (math.sin(m5) * L1 + math.sin(m4 + m5) * L2 + math.sin(m3 + m4 + m5) * L3)
-EQ_Z = lambda m3, m4, m5: H + math.cos(m5) * L1 + math.cos(m4 + m5) * L2 + math.cos(m3 + m4 + m5) * L3
+EQ_X = lambda m3, m4, m5, m6: math.cos(m6) * (math.sin(m5) * L1 + math.sin(m4 + m5) * L2 + math.sin(m3 + m4 + m5) * L3 - math.cos(m3 + m4 + m5) * L_3_1)
+EQ_Y = lambda m3, m4, m5, m6: math.sin(m6) * (math.sin(m5) * L1 + math.sin(m4 + m5) * L2 + math.sin(m3 + m4 + m5) * L3 - math.cos(m3 + m4 + m5) * L_3_1)
+EQ_Z = lambda m3, m4, m5: H + math.cos(m5) * L1 + math.cos(m4 + m5) * L2 + math.cos(m3 + m4 + m5) * L3 + math.sin(m3 + m4 + m5) * L_3_1
 EQ_THETA = lambda m3, m4, m5: m3 + m4 + m5
 # fmt: on
 
@@ -101,7 +102,7 @@ def open_claw(arm):
 
 
 def close_claw(arm):
-    move(arm, 1, 2000)
+    move(arm, 1, 2200)
 
 
 def vertical_claw(arm):
@@ -124,24 +125,28 @@ def calc_angles(x: float, y: float, z: float) -> tuple[float, float, float, floa
         eq_1 = EQ_X(m3, m4, m5, m6) - x
         eq_2 = EQ_Y(m3, m4, m5, m6) - y
         eq_3 = EQ_Z(m3, m4, m5) - z
-        eq_4 = 10 * (abs(EQ_THETA(m3, m4, m5)) - math.pi)
+        eq_4 = abs(EQ_THETA(m3, m4, m5)) - math.pi
 
-        return (eq_1, eq_2, eq_3, eq_4)
+        return (10 * eq_1, 10 * eq_2, 10 * eq_3, eq_4)
 
     result = least_squares(
         equations,
         [sum(b) / 2 for b in BOUNDS],
         bounds=[*zip(*BOUNDS)],
     )
-    print(result)
 
     return tuple(math.degrees(m) for m in result.x)
 
 
-def move_to_default(arm: xarm.Controller) -> None:
+def move_to_default(arm: xarm.Controller, open_claw: bool = True) -> None:
     for servo, (*_, default) in LIMITS.items():
         CURRENT_POSITIONS[servo] = default
+
+        if servo == 1 and not open_claw:
+            continue
+
         arm.setPosition(servo, default, wait=False)
+        time.sleep(0.5)
 
 
 X_OFFSET = -2
@@ -157,9 +162,13 @@ ADJUSTMENTS = (
     (Z_OFFSET, Z_COEFFICIENT),
 )
 
-CLAW_ADJUST_TIME = 0.8
-def move_to_position(arm: xarm.Controller, pos: tuple[float, float, float], adjust_claw=False) -> None:
-    pos = tuple(c * p + o for (o, c), p in zip(ADJUSTMENTS, pos))
+CLAW_ADJUST_TIME = 0.95
+
+
+def move_to_position(
+    arm: xarm.Controller, pos: tuple[float, float, float], open=False, close=False
+) -> None:
+    # pos = tuple(c * p + o for (o, c), p in zip(ADJUSTMENTS, pos))
     m3, m4, m5, m6 = calc_angles(*pos)
     m3r, m4r, m5r, m6r = tuple(math.radians(x) for x in (m3, m4, m5, m6))
     x = EQ_X(m3r, m4r, m5r, m6r)
@@ -167,23 +176,29 @@ def move_to_position(arm: xarm.Controller, pos: tuple[float, float, float], adju
     z = EQ_Z(m3r, m4r, m5r)
     theta = math.degrees(EQ_THETA(m3r, m4r, m5r))
     print(f"({x:.2f}, {y:.2f}, {z:.2f}) {theta:.2f}")
-    print()
     servos = list(zip(range(3, 7), (m3, m4, m5, m6)))
+
     durations = []
-    
+
     for servo, angle in servos:
         durations.append(move(arm, servo, angle_to_position(servo, angle)))
 
-    max_duration = max(durations)
-    
-    if adjust_claw:
-        time.sleep(CLAW_ADJUST_TIME * max_duration / 1000)
+    max_duration = max(durations) / 1000
+
+    if close:
+        time.sleep(CLAW_ADJUST_TIME * max_duration)
         close_claw(arm)
+        time.sleep(1)
+    elif open:
+        time.sleep(max_duration + 0.1)
+        open_claw(arm)
+    else:
+        time.sleep(max_duration + 0.1)
 
 
-MIN_DURATION = 800
-MAX_DURATION = 5000
-SPEED = 30  # degrees per second
+MIN_DURATION = 500
+MAX_DURATION = 1000
+SPEED = 60  # degrees per second
 
 
 def move(arm, servo, target_pos):
@@ -194,7 +209,10 @@ def move(arm, servo, target_pos):
     duration = min(
         max(int(delta_angle * (1 / SPEED) * 1000), MIN_DURATION), MAX_DURATION
     )
-    print(servo, delta_angle, duration / 1000)
+    # target_pos = max(target_pos, 0)
+    print(
+        f"{servo=}, {current_pos=}, {target_pos=}, {current_angle=}, {target_angle=}, {duration=}"
+    )
 
     CURRENT_POSITIONS[servo] = target_pos
     arm.setPosition(servo, target_pos, duration=duration, wait=False)
@@ -202,7 +220,17 @@ def move(arm, servo, target_pos):
     return duration
 
 
+BINS = [
+    (-30, 25, 25),
+    (-15, 15, 25),
+    (-20, 3, 25),
+]
 arm = xarm.Controller("USB")
+
+# move_to_default(arm)
+# for bin in BINS:
+#     input()
+#     move_to_position(arm, bin)
 
 # while True:
 #     inp = input("Enter a position to move to: ")
@@ -210,8 +238,16 @@ arm = xarm.Controller("USB")
 #         move_to_default(arm)
 #         continue
 #     pos = tuple(float(i) for i in inp.split())
-#     move_to_position(arm, pos)
 
+
+def pickup(arm, pos, bin_num):
+    move_to_position(arm, (pos[0], pos[1], pos[2] + 10))
+    move_to_position(arm, pos, close=True)
+    move_to_position(arm, (pos[0], pos[1], pos[2] + 10))
+    move_to_default(arm, open_claw=False)
+    move_to_position(arm, BINS[bin_num], open=True)
+    move_to_default(arm)
+    
 
 def main() -> None:
     print("Video capture set up")
@@ -219,11 +255,12 @@ def main() -> None:
     model = YOLO("yolov8N.pt")
     print("Model loaded")
 
-    while True:
-        input("Press enter to move to default position...")
-        move_to_default(arm)
+    # input("Press enter to move to default position...")
+    move_to_default(arm)
 
-        input("Press enter to capture a frame...")
+    while True:
+
+        # input("Press enter to capture a frame...")
         ret, frame = cv2.VideoCapture(1).read()
         if not ret:
             break
@@ -243,18 +280,14 @@ def main() -> None:
             f"{model.names[ box.cls.item()]}: {box.conf.item():.2f}",
             color=(0, 0, 255),
         )
-        cv2.imshow("Detection Results", frame)
+        # cv2.imshow("Detection Results", frame)
 
-        if cv2.waitKey(50) & 0xFF == ord("q"):
-            break
+        # if cv2.waitKey(50) & 0xFF == ord("q"):
+        #    break
 
         input("Press enter to move to selected piece...")
-        pos_1 = (*bounding_box_to_position(box.xywhn[0]), 5)
-        pos_2 = (pos_1[0], pos_1[1], 0)
-        move_to_position(arm, pos_1)
-        time.sleep(0.5)
-        move_to_position(arm, pos_2, adjust_claw=True)
-        time.sleep(0.5)
+        pos = (*bounding_box_to_position(box.xywhn[0]), 0)
+        pickup(arm, pos, 0)
 
 
 if __name__ == "__main__":
